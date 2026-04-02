eb55720eb0214ae76b8f31d4e8445d459eb34e9f
Saur0w <sthapliyal085@gmail.com>
2026-04-02 11:33:14 +0530

    "starting out"

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import styles from "./style.module.scss";
+
+export default function Home() {
+    return (
+        <section className={styles.home}>
+
+        </section>
+    )
+}
